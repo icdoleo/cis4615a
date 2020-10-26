3602b51a95b7577f9b2ac29d76d8dee954f84db1
icdoleo <46574427+icdoleo@users.noreply.github.com>
2020-10-25 20:53:17 -0400

    Update R14_SER00_J.java

--- R14_SER00_J.java
+++ R14_SER00_J.java
@@ -1,9 +1,11 @@
 // Rule 14. Serialization (SER)
 // SER00-J. Enable serialization compatibility during class evolution
 
-// Noncompliant Code Example 
+// Compliant Solution (serialVersionUID) 
 
 class GameWeapon implements Serializable {
+  private static final long serialVersionUID = 24L;
+ 
   int numOfWeapons = 10;
          
   public String toString() {
